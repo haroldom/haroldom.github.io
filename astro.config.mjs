9d8871fb5bd8f0d8df92b119980c2d993c9d03b8
Harold Ormeño <haroldmeneses2006@gmail.com>
2026-08-21 01:12:37 -0500

    Changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://haroldormeno.com',
+  site: 'https://haroldom.github.io',
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'es'],
